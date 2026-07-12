1daf3b7106bfed02edcf741543583fa7249b88a6
czx <yun71212@qq.com>
2026-07-13 00:21:13 +0800

    字符串拼接的练习

---
+++ 14_字符串加号拼接练习.py
@@ -0,0 +1,11 @@
+"""
+字符串加号拼接练习
+"""
+
+
+money = 2023123
+name = "张三"
+salary = 10031231
+
+info = "我是"+name+", 钱包有"+str(money)+"元，但是今天发放了工资"+str(salary)+"元，目前钱包有"+str(money+salary)+"元"
+print(info)
