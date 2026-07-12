8ebbaa10bb5cb8913873cfa1e49614476efc813a
czx <yun71212@qq.com>
2026-07-12 22:38:08 +0800

    变量的学习与应用

---
+++ 04_变量的基础使用.py
@@ -0,0 +1,21 @@
+"""
+代码讲演示变量的基础应用
+"""
+
+
+# 将50这个数值，存入变量money中
+money = 50
+name = "周杰伦"
+
+# print可以将字面量的内容在屏幕显示，那么变量呢？
+# 可以的
+# 也能证明money和50是等价的，即证明money内部存储了50这个值
+print(money)
+print(name)
+
+money = 100
+name = "124"
+
+
+print(money)
+print(name)
---
+++ 05_变量的重复应用.py
@@ -0,0 +1,27 @@
+
+name = "周杰伦"
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+
+print("---------------------------")
+
+name = "蔡依林"
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
