f18a2600f8ad1b7cfd0a8f91b44b3dba98df7eee
czx <yun71212@qq.com>
2026-07-27 23:02:59 +0800

    存储方式遍历练习

---
+++ 00案例实践与误区解答/04_存储方式遍历.py
@@ -0,0 +1,20 @@
+"""
+
+
+"""
+
+
+
+
+
+# 存储方式1: 列表
+class_1 = [
+    {"name":"小张","age":18},
+    {"name":"小潘","age":28},
+    {"name":"小文","age":38}
+]
+
+
+for stu in class_1:
+    if stu["name"] == "小张":
+        print(stu["name"],stu["age"])
